08b81e50dfa22f00156a98086c774c0f46dc22ee
Maylan <maylan.gomes@epitech.eu>
2024-05-08 00:38:34 +0200

    fix: def constante pour eTargetValue car pb déploiement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,15 +74,15 @@ const Quiz = () => {
         <p className='text-sm'>Astuce : la taille moyenne d'une pomme est de 12,5cm.</p>
         <div className="mt-3">
           <div className="flex items-center space-x-2">
-            <input type="radio" id="3m" value="3m" {...register("hauteurPanier")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+            <input type="radio" id="3m" value="3m" {...register("Question3")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
             <label htmlFor="3m" className="ml-2 text-lg">3 mètres</label>
           </div>
           <div className="flex items-center space-x-2">
-            <input type="radio" id="3m05" value="3m05" {...register("hauteurPanier")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+            <input type="radio" id="3m05" value="3m05" {...register("Question3")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
             <label htmlFor="3m05" className="ml-2 text-lg">3,05 mètres</label>
           </div>
           <div className="flex items-center space-x-2">
-            <input type="radio" id="3m10" value="3m10" {...register("hauteurPanier")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+            <input type="radio" id="3m10" value="3m10" {...register("Question3")} className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
             <label htmlFor="3m10" className="ml-2 text-lg">3,10 mètres</label>
           </div>
         </div>
