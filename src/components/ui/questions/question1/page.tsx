535a3df004c314cb3fdc2ddce30dcf03237b1041
Maylan <maylan.gomes@epitech.eu>
2024-05-13 09:58:41 +0200

    delete : plus grand dans la question 1

--- src/components/ui/questions/question1/page.tsx
+++ src/components/ui/questions/question1/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 const InputQuestion1: React.FC<any> = ({ register, errors }) => {
   return (
     <div className="mt-8">
-      <h2 className="text-lg font-semibold">Question 1 : Nom du plus grand joueur avec le meilleur pourcentage à 3 points de l'histoire sur une saison</h2>
+      <h2 className="text-lg font-semibold">Question 1 : Nom du joueur avec le meilleur pourcentage à 3 points de l'histoire sur une saison</h2>
       <p className='text-sm'>Indice : ce n'est pas Westbrook.</p>
       <Input type="text" {...register("question1")} className="mt-1 w-full bg-gray-200 text-gray-800 px-3 py-2 rounded-lg" />
       {errors.question1 && errors.question1.message?.toString()}
