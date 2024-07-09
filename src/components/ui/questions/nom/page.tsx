cc8fe6af780f17ed4a607d2c4e66014c6c64ddfc
Maylan <maylan.gomes@epitech.eu>
2024-07-09 18:30:42 +0200

    fix: ajout italic prénom

--- src/components/ui/questions/nom/page.tsx
+++ src/components/ui/questions/nom/page.tsx
@@ -5,7 +5,7 @@ const InputNom: React.FC<any> = ({ register, errors }) => {
   return (
     <div>
     <h2 className="text-xl font-bold text-primary">Prénom</h2>
-    <p className='text-sm'>Si vous souhaitez que le score soit enregistré.</p>
+    <p className='text-sm italic'>Si vous souhaitez que le score soit enregistré.</p>
     <Input type="text" {...register("nom")} className="mt-1 w-full bg-gray-200 text-gray-800" />
     {errors.nom && errors.nom.message?.toString()}
   </div>
